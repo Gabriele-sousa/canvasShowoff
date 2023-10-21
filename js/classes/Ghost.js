5fb4926d60b538e51b0579b0aad2faca0d59fb7a
filipe <filipe.goncalves29@etec.sp.gov.br>
2023-10-21 16:14:30 -0300

    Movimento vertical do boneco

--- js/classes/Ghost.js
+++ js/classes/Ghost.js
@@ -1,22 +1,40 @@
 class Ghost{
-    constructor(x, y, w, h, obj){
+    constructor(x, y, h, vel){
         this.x = x;
         this.y = y;
-        this.w = w;
         this.h = h;
-        this.obj = obj;
+        this.v = vel;
     }
 
-    desenhar(x, y){
-        ctx.fillStyle = "white";
-        ctx.fillRect(x, y, this.w, this.h)
+    desenhar(x, y, c){
+        gst.fillStyle = c;
+        this.x = x;
+        this.y = y;
+        gst.fillRect(this.x, this.y, 40, 40)
     }
     mover(tecla){
-        if (tecla === "w"){
+        this.trava();
+        if (tecla === "d"){
+            //apaga o ultimo desenho
+            gst.fillStyle = "black";
+            gst.fillRect(this.x, this.y, -1000, 40);
 
+            this.desenhar(this.x, this.y, "white");
+            gst.translate(20,0);
+        }else if (tecla === "a"){
+            //apaga o ultimo desenho
+            gst.fillStyle = "black";
+            gst.fillRect(this.x, this.y, 1000, 40);
+            
+            this.desenhar(this.x, this.y, "white");
+            gst.translate(-20,0);
         }
     }
-
+    trava(x, y){
+        if(this.x > x && this.y > y){
+            return;
+        }
+    }
     mudarCor(tecla){
         
     }
--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
-const obj = document.getElementById('obj');
+const gst = canvas.getContext('2d');
 const ghost = new Ghost();
 
 canvas.width = window.innerWidth;
@@ -8,28 +8,19 @@ canvas.height = window.innerHeight;
 w = canvas.width;
 h = canvas.height;
 
-ghost.obj = obj;
-ghost.w = 50;
-ghost.h = 50;
-
-obj.style.width = ghost.w + "px";
-obj.style.height = ghost.h + "px";
-obj.style.position = "absolute";
-ghost.w = parseInt(obj.style.width);
-ghost.w = parseInt(obj.style.height);
-
 function piso(x, y){
     ctx.fillStyle = "white";
     ctx.fillRect(x, y, 150, 15);
+    ghost.trava(x, y)
 }
 
 ctx.fillStyle = "black";
 ctx.fillRect(0, 0, w, h);
 
-ghost.desenhar(250, 850);
+ghost.desenhar(250, 850, "white");
 piso(300,800);
 
 document.addEventListener("keypress", function(e){
-    obj.style.translate = "300px, 500px";
-    console.log(parseInt(obj.style.width));
+    console.log(e.key)
+    ghost.mover(e.key);
 });
